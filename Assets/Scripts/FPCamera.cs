4352f9f368ec4b7b3ddf6a879948be0336198316
PabloArregui <pab0006@alu.medac.es>
2023-11-08 09:31:19 +0100

    MovmentCharacter Termincado

--- Assets/Scripts/FPCamera.cs
+++ Assets/Scripts/FPCamera.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class FPCamera : MonoBehaviour
 {
-    //Aqui tienes todas las variables que vamos a utilizar
+    //Variables 
     public Vector2 sensibility;
     private new Transform camera;
     private new Rigidbody rigidbody;
     public float movmentSpeed;
     public float rayDistance;
 
+    //Variables para GUI
     public Texture2D puntero;
+
+    //Variables para la interaccion de obejetos
+    public GameObject hand;
+    private GameObject objetoMano = null;
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +34,8 @@ public class FPCamera : MonoBehaviour
     {
         CameraControl();
         Movmetcharacter();
-        Rayocamera();
+        //Rayocamera();
+        //SoltarObjeto();
     }
 
     //Control de la camara con el raton
@@ -77,15 +83,60 @@ public class FPCamera : MonoBehaviour
     }
 
     //Funcionalidades del Rayo
+    /*
     public void Rayocamera()
     {
+        //Para que se pueda ver el Rayo
         Debug.DrawRay(camera.position, camera.forward * rayDistance, Color.red);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, rayDistance))
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                RecogerObjeto();
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.Q))
+            {
+                SoltarObjeto();
+            }
+        }
     }
 
+    private void RecogerObjeto(GameObject objeto)
+    {
+        if (objeto.gameObject.CompareTag("Objeto"))
+        {
+            objeto.GetComponent<Rigidbody>().useGravity = false;
+            objeto.GetComponent<Rigidbody>().isKinematic = true;
+            objeto.transform.position = hand.transform.position;
+            objeto.gameObject.transform.SetParent(hand.gameObject.transform);
+            objetoMano = objeto.gameObject;
+
+        }
+    }
+
+    private void SoltarObjeto()
+    {
+        if(objetoMano != null)
+        {
+            objetoMano.GetComponent<Rigidbody>().useGravity = true;
+            objetoMano.GetComponent<Rigidbody>().isKinematic = false;
+            objetoMano.gameObject.transform.SetParent(null);
+            objetoMano = null;
+        }
+    }
+    */
 
     private void OnGUI()
     {
         //Para que le podamos poner puntero
         Rect rect = new Rect(Screen.width / 2, Screen.height / 2, puntero.width, puntero.height);
+        GUI.DrawTexture(rect, puntero);
     }
 }
