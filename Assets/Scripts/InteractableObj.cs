f90fdfa12df76b58f49c5aaf490cedd5aa369755
JonDLCruz <149765741+JonDLCruz@users.noreply.github.com>
2023-11-09 13:38:38 +0100

    Creación de DB Provisional

--- Assets/Scripts/InteractableObj.cs
+++ Assets/Scripts/InteractableObj.cs
@@ -8,10 +8,12 @@ public class InteractableObj
     public string name;
     public string descr;
     public string AnimationPath;
+     
     public InteractableObj(string _name, string _descr, string _animationPath)
     {
         this.name = _name;
         this.descr = _descr;
         AnimationPath = _animationPath;
     }
+   
 }
